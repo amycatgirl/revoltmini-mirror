643fa645d291ae53ee27ef0d93007c69f1860954
Amy <amycatgirl@disroot.org>
2023-12-10 16:15:03 -0400

    refactor!: port MDSymbols component to lit

--- scripts/components/mdsymbols.js
+++ scripts/components/mdsymbols.js
@@ -1,46 +1,41 @@
-class MDSymbols extends HTMLElement {
-  static get observedAttributes() {
-    return ["name", "fill", "weight", "emphasis", "optical-size"];
+import {css, html, LitElement, unsafeCSS} from "lit";
+
+class MDSymbols extends LitElement {
+
+  static properties = {
+    name: { type: String },
+    fill: { type: Number },
+    emphasis: { type: Number },
+    opticalSize: { type: Number, attribute: "optical-size"},
+    weight: { type: Number }
   }
+
+  static styles = css`
+    .material-symbols-outlined {
+        font-variation-settings:
+        'FILL' ${unsafeCSS(this.fill)},
+        'wght' ${unsafeCSS(this.weight)},
+        'GRAD' ${unsafeCSS(this.emphasis)},
+        'opsz' ${unsafeCSS(this.opticalSize)}
+      }
+  `
 
   constructor() {
     super();
-    this.attachShadow({ mode: "open" });
+
+    this.name = "info"
+
+    this.fill = 0;
+    this.weight = 400;
+    this.emphasis = 0;
+    this.opticalSize = 24;
   }
 
-  connectedCallback() {
-    const shadowDOM = this.shadowRoot;
-    const name = this.getAttribute("name");
-    if (!name) return;
-
-    const fill = this.getAttribute("fill") | 0;
-    const weight = this.getAttribute("weight") | 400;
-    const emphasis = this.getAttribute("emphasis") | 0;
-    const opticalSize = this.getAttribute("optical-size") | 24;
-
-    const style = document.createElement("style");
-    const link = document.createElement("link");
-    link.href =
-      "https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined";
-    link.rel = "stylesheet";
-
-    style.innerText = `
-      .material-symbols-outlined {
-        font-variation-settings:
-        'FILL' ${fill},
-        'wght' ${weight},
-        'GRAD' ${emphasis},
-        'opsz' ${opticalSize}
-      }
-    `;
-
-    const icon = document.createElement("span");
-    icon.classList.add("material-symbols-outlined");
-    icon.innerText = name;
-
-    shadowDOM.appendChild(icon);
-    shadowDOM.appendChild(style);
-    shadowDOM.appendChild(link);
+  render() {
+    return html`
+      <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined">
+      <span class="material-symbols-outlined">${this.name}</span>
+    `
   }
 }
 
